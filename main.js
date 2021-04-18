22f288c5ba5668c19bcf73d0de296a5969c8b233
An Do Van (Intern-Tech Pools-Technology-VND HO) <an.do@vndirect.com.vn>
2021-04-18 14:08:05 +0700

    ad

---
+++ main.js
@@ -0,0 +1,32 @@
+const jsonServer = require('json-server')
+const queryString = require('query-string');
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+router.render = (req, res) => {
+  const header = res.getHeaders();
+  const totalCountHeader = header['x-total-count'];
+  if (req.method === 'GET' && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 7,
+        _totalRows: Number.parseInt(totalCountHeader)
+
+      }
+    }
+    return res.jsonp(result);
+  }
+  res.jsonp(res.locals.data);
+}
+// Use default router
+server.use('/api', router)
+server.listen(3001, () => {
+  console.log('JSON Server is running')
+})
